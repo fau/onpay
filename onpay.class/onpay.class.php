4bdb461efef21a1cc42f1c31c3de90fddb06be37
Alexey Fedorov <fau@mail.ru>
2015-07-07 23:56:56 +0400

    db interface declaration

--- onpay.class/onpay.class.php
+++ onpay.class/onpay.class.php
@@ -1,5 +1,27 @@
 <?php
 
+
+/**
+ * Интерфейс класса базы данных для взаимодействия с API Onpay
+ *
+ * @author fau
+ * @version 1.0
+ * @filesource
+ * @namespace Onpay
+ *
+ */
+interface ionepay_db
+{
+    public function __construct($dbname);
+    public function init();
+    public function lastErrorMsg();
+    public function insertOrder($summ, $user_email, $date);
+    public function lastInsertRowID();
+    public function updateOrder($order_id, $onpay_id, $payed_date);
+    public function orderStatus($order_id);
+    public function findOrder($order_id, $summ);
+}
+
 /**
  * Класс взаимодействия с API Onpay
  *
